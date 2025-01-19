941865e0b8940308ffd0a06c2c1e23a885ffe233
电视宝 www.itvapp.net <service@itvapp.net>
2025-01-19 19:30:04 +0800

    Update frp_script.sh

--- trunk/user/frp/frp_script.sh
+++ trunk/user/frp/frp_script.sh
@@ -8,45 +8,62 @@ mkdir -p /tmp/frp
 cat > "/tmp/frp/frpc.toml" <<-\EOF
 # ==========客户端配置：==========
 [common]
-# IPv6的字面地址或主机名必须用方括号括起来
-# 例如"[::1]:80"、"[ipv6-host]:http"或"[ipv6-host%zone]:80"
+# IPv6必须用方括号括起来，例如"[::1]:80"、"[ipv6-host]:http"或"[ipv6-host%zone]:80"
 # 对于单个"server_addr"字段，不需要方括号，如"server_addr = ::"
-server_addr = 0.0.0.0
-server_port = 7000
-# 认证令牌
-token = 12345678
+server_addr = "0.0.0.0"
+
+# 连接端口
+server_port = "7000"
+
+# 特权模式密钥，客户端连接到FRPS服务端的验证密钥
+token = "12345678"
+
 # 用于连接服务器的通信协议, 支持tcp、kcp和websocket
 protocol = tcp
+
 # 连接服务器时等待连接完成的最大时间。默认值为18秒
 # dial_server_timeout = 18
+
 # 预先建立的连接数量，默认值为0
 pool_count = 8
+
 # 是否启用tcp流复用，默认为true，必须与frps保持一致
 tcp_mux = true
+
 # 指定tcp复用的保活间隔时间，仅在tcp_mux为true时有效
 tcp_mux_keepalive_interval = 60
+
 # 你的代理名称将被更改为{user}.{proxy}
 user = Aicss_Net
+
 # 决定首次登录失败时是否退出程序，否则将持续尝试重新登录frps
 login_fail_exit = true
-# 如果tls_enable为true，frpc将通过tls连接frps
-tls_enable = true
-# tls_cert_file = client.crt
-# tls_key_file = client.key
-# tls_trusted_ca_file = ca.crt
-# tls_server_name = example.com
-[web01]
+
+[Aicss_web01]
+# 特权模式
+privilege_mode = true
+
+# 穿透协议类型，可选：tcp，udp，http，https，stcp，xtcp
 type = http
-local_ip = 127.0.0.1
+
+# 要解析到的内网IP
+local_ip = 192.168.168.102
+
+# 本地监听端口，通常有ssh端口22，远程桌面3389等等
 local_port = 80
-use_encryption = false
-use_compression = true
-# 如果frps的域名是Aicss.Net，那么可以通过URL http://web01.Aicss.Net访问
+
+# 如果frps的域名是Aicss.Net，那么可以通过URL http://web01.Aicss.Net 访问
 subdomain = web01
+
+# 你自己的域名
 custom_domains = web01.yourdomain.com
-# 带有"header_"前缀的参数将用于更新http请求头
-header_X-From-Where = frp
-health_check_type = http
+
+# 对传输内容进行压缩
+use_encryption = true
+
+# 将 frpc 与 frps 之间的通信内容加密传输
+use_compression = true
+
 # ====================
 EOF
 

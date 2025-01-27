f776dcae2a0bc3db93d3192c537903d81f16ecee
电视宝 www.itvapp.net <service@itvapp.net>
2025-01-27 20:09:30 +0800

    Update setip.sh

--- trunk/custom/scripts/setip.sh
+++ trunk/custom/scripts/setip.sh
@@ -8,8 +8,8 @@ oc2=${x%%.*}
 x=${x#*.*}
 oc3=${x%%.*}
 dhcpfrom=$oc1"."$oc2"."$oc3".100"
-dhcpto=$oc1"."$oc2"."$oc3".244"
-sed -i "s/\"192.168.2.1\"/\"$lanip\"/g" $defaultfile
-sed -i "s/\"192.168.2.100\"/\"$dhcpfrom\"/g" $defaultfile
-sed -i "s/\"192.168.2.244\"/\"$dhcpto\"/g" $defaultfile
-sed -i "s/192.168.2.1/$lanip/" $dicfile
+dhcpto=$oc1"."$oc2"."$oc3".238"
+sed -i "s/\"192.168.168.1\"/\"$lanip\"/g" $defaultfile
+sed -i "s/\"192.168.168.100\"/\"$dhcpfrom\"/g" $defaultfile
+sed -i "s/\"192.168.168.238\"/\"$dhcpto\"/g" $defaultfile
+sed -i "s/192.168.168.1/$lanip/" $dicfile
